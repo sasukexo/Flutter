5681f6cae00f6f5c5e0c379e22316865b1966294
Sasukexo <aswinash1002@gmail.com>
2025-03-26 11:25:22 +0530

    changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'pages/firstpage.dart';
+import 'pages/secondpage.dart';
+import 'pages/thirdpage.dart';
+import 'pages/fourthpage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,9 +24,48 @@ class HomePage extends StatelessWidget {
               );
             },
             child: Text('Go to First Page'),
+          ),
+           TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondPage()),
+              );
+            },
+            child: Text('Go to Second Page'),
+          ),
+           ElevatedButton.icon(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ThirdPage()),
+              );
+            },
+            icon: Icon(Icons.navigate_next),
+            label: Text('Go to Third Page'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FourthPage()),
+              );
+            },
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                'Go to Fourth Page',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           )
+       
         ]
-      )
+      ),
    );
   }
 }
